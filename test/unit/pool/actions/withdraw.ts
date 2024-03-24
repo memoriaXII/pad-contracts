d7f41aa6c3d7ef7a2328773e6e94ddf5807d5a47
memoriaXII <chenglei.chou@gmail.com>
2024-03-25 01:52:28 +0800

    ct

--- test/unit/pool/actions/withdraw.ts
+++ test/unit/pool/actions/withdraw.ts
@@ -1,18 +1,18 @@
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import type { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
 import { time } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
-
-
+import hre, { ethers } from "hardhat";
 
 import { Errors } from "../../../shared/errors";
 
 export default function shouldBehaveLikeWithdraw(): void {
   context("when called too soon", function () {
-    it("reverts", async function () {
-      // await expect(this.contracts.lock.withdraw()).to.be.revertedWithCustomError(
-      //   this.contracts.lock,
-      //   Errors.Lock_CannotWithdrawYet
-      // );
+    it("Should properly initialize ", async () => {
+      const snapshot = await hre.network.provider.send("evm_snapshot");
+      const signers = await ethers.getSigners();
+      const deployer: SignerWithAddress = signers[0];
     });
+    it("Should properly create presale", async () => {});
   });
 }
