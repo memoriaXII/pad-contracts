1720556d1cfb37986ab9f1b6df4062c5810cc9b9
memoriaXII <chenglei.chou@gmail.com>
2024-03-28 16:25:02 +0800

    ct

--- test/unit/pool/actions/withdraw.ts
+++ test/unit/pool/actions/withdraw.ts
@@ -4,10 +4,15 @@ import hre, { ethers } from "hardhat";
 import { hardcap, listingRate, presaleRate, types, value, vesting } from "../../../../constants";
 import { Errors } from "../../../shared/errors";
 
+export const advanceBlocks = async (blockNumber: number) => {
+  while (blockNumber > 0) {
+    blockNumber--;
+    await ethers.provider.send("evm_mine", []);
+  }
+};
 export default function shouldBehaveLikeWithdraw(): void {
   context("withdraw", function () {
     it("Should properly create presale", async function () {
-      // const snapshot = await hre.network.provider.send("evm_snapshot");
       const domain = {
         name: "EIP712-Derive",
         version: "1",
@@ -27,11 +32,44 @@ export default function shouldBehaveLikeWithdraw(): void {
       const usersTokenAmount = hardcap * BigInt(presaleRate); // Convert presaleRate to a bigint
       const liquidityTokenAmount = hardcap * BigInt(listingRate);
       const totalTokenAmount = usersTokenAmount + liquidityTokenAmount;
-
-      console.log(
-        await this.contracts.poolManager.getAddress(),
-        await this.contracts.pool.getAddress()
+      // Create presale
+      await this.contracts.poolManager.connect(wallet).createPresale(
+        {
+          ...value,
+          currency: await this.contracts.mockERC20.getAddress(),
+        },
+        vesting,
+        signature
       );
+      const proxyAddress = await this.contracts.poolManager.presales(0);
+      const poolBalance = await this.contracts.mockERC20.balanceOf(proxyAddress);
+      expect(poolBalance.toString()).to.equal(totalTokenAmount.toString());
+    });
+    it("Should not contribute before start time", async function () {
+      // const snapshot = await hre.network.provider.send("evm_snapshot");
+      // const proxyAddress = await this.contracts.poolManager.presales(0);
+      const snapshot = await hre.network.provider.send("evm_snapshot");
+      // accumulate interest
+      // await advanceBlocks(1000);
+      const domain = {
+        name: "EIP712-Derive",
+        version: "1",
+        chainId: 31337, //Hardhat mainnet-fork chain id
+        verifyingContract: await this.contracts.poolManager.getAddress(),
+      };
+      const deployer = await this.contracts.poolManager.owner();
+      const wallet = await hre.ethers.getSigner(deployer);
+      const signature = await wallet.signTypedData(domain, types, {
+        ...value,
+        currency: await this.contracts.mockERC20.getAddress(),
+      });
+      await this.contracts.pool.initialize(await this.contracts.poolManager.getAddress());
+      await expect(
+        this.contracts.pool.initialize(await this.contracts.poolManager.getAddress())
+      ).to.be.revertedWithCustomError(this.contracts.pool, Errors.Pool_AlreadyInitialized);
+      const usersTokenAmount = hardcap * BigInt(presaleRate); // Convert presaleRate to a bigint
+      const liquidityTokenAmount = hardcap * BigInt(listingRate);
+      const totalTokenAmount = usersTokenAmount + liquidityTokenAmount;
       // Create presale
       await this.contracts.poolManager.connect(wallet).createPresale(
         {
@@ -49,6 +87,14 @@ export default function shouldBehaveLikeWithdraw(): void {
         poolBalance.toString(),
         "totalTokenAmount",
         totalTokenAmount.toString()
+      );
+      const proxy = await ethers.getContractAt("contracts/pools/Pool.sol:Pool", proxyAddress);
+      const contributeAmount = ethers.parseEther("0.5");
+      const [user1] = await ethers.getSigners();
+      // accumulate interest
+      await advanceBlocks(1000);
+      await expect(proxy.connect(user1).contribute({ value: contributeAmount })).to.be.revertedWith(
+        "The presale is not active at this time."
       );
     });
   });
--- test/unit/pool/index.ts
+++ test/unit/pool/index.ts
@@ -5,20 +5,13 @@ import { poolFixture } from "./fixtures/pool.fixture";
 
 export function Pool(): void {
   describe("Pool", function () {
-    // beforeEach(async function () {
-    //   const { pool, padLock, poolManager, token } = await this.loadFixture(poolFixture);
-    //   this.contracts.pool = pool;
-    //   this.contracts.padLock = padLock;
-    //   this.contracts.poolManager = poolManager;
-    //   this.contracts.mockERC20 = token;
-    //   this.snapshotId = await network.provider.send("evm_snapshot");
-    //   await network.provider.send("evm_increaseTime", [900]);
-    //   await network.provider.send("evm_mine");
-    // });
-    // afterEach(async function () {
-    //   await network.provider.send("evm_revert", [this.snapshotId]);
-    // });
-
+    beforeEach(async function () {
+      const { pool, padLock, poolManager, token } = await this.loadFixture(poolFixture);
+      this.contracts.pool = pool;
+      this.contracts.padLock = padLock;
+      this.contracts.poolManager = poolManager;
+      this.contracts.mockERC20 = token;
+    });
     // shouldBehaveLikeLockContract();
     describe("View Functions", function () {
       describe("# read initial parameters", function () {
